efb91d68eeaf2ce1333bc1d7ab7004267067b142
VishnuPrasanth-A <150049723+VishnuPrasanth-A@users.noreply.github.com>
2024-03-16 21:32:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Founder and CEO of Solving Bees🐝"," Frontend Developer !", " Student !", " Web Developer !", " Article Writer !", " Data Analyst !"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
